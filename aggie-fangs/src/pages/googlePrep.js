4c154cee5b1217c7e1e5ccc453fce696aa84d7f1
Saket Gayake <saket.gayake@tamu.edu>
2022-04-19 17:00:54 -0500

    youtube player working for google

--- aggie-fangs/src/pages/googlePrep.js
+++ aggie-fangs/src/pages/googlePrep.js
@@ -2,7 +2,9 @@ import React, {useState} from 'react'
 import Form from '../components/Form'
 import Notion from '../components/Notion'
 import ReviewList from '../components/ReviewList'
+import VideoComp from '../components/VideoPlayer/VideoComp'
 import { v4 as uuidv4 } from "uuid";
+
 const GooglePrep = () => {
   const [reviews, setReviews] = useState([])
   const [form, setForm] = useState({company: "", review: "", id: uuidv4()});
@@ -11,7 +13,7 @@ const GooglePrep = () => {
         <h1>Google Interview Feedback</h1>
         <Form form = {form} reviews = {reviews} setForm={setForm} setReviews={setReviews}/>
         <ReviewList reviews = {reviews} setReviews = {setReviews}/>
-        <Notion/> 
+        <Notion/>
     </div>
   )
 }
