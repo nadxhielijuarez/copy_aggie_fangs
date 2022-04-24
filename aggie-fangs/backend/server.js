db4d33b41debebce808710f38a8897de33c0f892
nadxhieli <84687270+nadxhielijuarez@users.noreply.github.com>
2022-04-23 19:16:18 -0500

    working Notion API Call
    Working from a different directory, was not able to push previous changes

---
+++ aggie-fangs/backend/server.js
@@ -0,0 +1,132 @@
+// form-tool-backend/server.js
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
+const app = express()
+const port = 3002
+
+const secretKey = 'secret_AFKZAuWeh8KSRFU7dK4vcdUTEQG1pb3CyQtwBIdj9Ws'
+NOTION_DATABASE_ID = '22f238cc864e4a1496e42e3d8a2c05c6'
+//email type: email
+NOTION_EMAIL_ID = 'S%3AUR'
+//name type: rich text
+NOTION_NAME_ID = 'Tqov'
+//tag type: multi_select
+NOTION_TAG_ID = '%5C~%7Cf'
+//down votes type: number
+NOTION_DVOTES_ID = 'g~OQ'
+//description type: rich text
+NOTION_DESCRIPTION_ID = 'h%5Da%3E'
+//up votes type: number
+NOTION_UPVOTES_ID = 'k_Eb'
+//title type: title
+NOTION_TITLE_ID = 'title'
+
+
+app.use(express.urlencoded({extended: true })); 
+
+app.use(express.json());   
+//properties: myProperties
+const myProperties = {
+      'title': {
+        title: [
+          {
+            type: "text",
+            text: {
+              content: "testing empty",
+            },
+          },
+        ],
+      },
+      'S%3AUR': {
+        email : 'testEmail@tamu.edu'
+      },
+      'Tqov': {
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: "testName",
+            },
+          },
+        ],
+      },
+      'h%5Da%3E': {
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: 'testDescription',
+            },
+          },
+        ],
+      },
+      'g~OQ': {
+        number: 10,
+      },
+      'k_Eb': {
+        number: 12,
+      },
+      
+  }
+// What we'll pass into axios
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: `Bearer ${secretKey}`,
+  'Notion-Version': '2022-02-22'
+}
+
+
+/* route to basic database info */
+app.get('/:database_id', async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  const { database_id } = req.params;
+  const resp = await axios({
+    method: 'GET',
+    url: 'https://api.notion.com/v1/databases/' + database_id,
+    headers
+  });
+  return res.json(resp.data);
+})
+ 
+/* route that allows us to enter info into the feedback form */
+app.post('/:database_id' , async (req, res) => {
+  const { database_id } = req.params
+  /* weird behaviour  */
+  var body = req.body //the object is listed as: { '{"title":"EMPTY TEST","email":"test@tamu.edu"}': '' } note the 1st key is the actual obj
+  var strData = Object.keys(body)[0] //obtains the first key, currently as a string 
+  var data = JSON.parse(strData) //actual object is now obtained
+  /* corrected the wierd behaviour*/
+  const { title, email } = data
+  res.header("Access-Control-Allow-Origin", "*");
+  await axios({
+    method: 'POST',
+    url: 'https://api.notion.com/v1/pages',
+    headers,
+    data: {
+      parent: { database_id },
+      properties: {
+        "title": { "title": [{ text: { content: title } }] },
+        'S%3AUR': { "email": email }
+      }
+    }
+  })
+  
+})
+
+app.use(cors({
+  origin: ['http://localhost:3002'],
+  credentials: true
+}))
+
+
+app.listen(port, () => {
+  console.log(`On port ${port}`)
+})
+
+
+
+/*NOTES:
+
+
+*/
--- aggie-fangs/src/components/Form.js
+++ aggie-fangs/src/components/Form.js
@@ -1,32 +1,126 @@
-import React, {useState} from 'react'
-import { uuid } from "uuidv4";
-const Form = () => {
-// const [form, setForm] = useState({Company: "", review: "", id: uuid()})
-//   const [reviews, setReviews] = useState([])
-//   const handleChange = e =>{
-//       const {name, value} = e.target
-//       setForm({...form, [name]:value })
-//   }
+import React from 'react'
+import { v4 as uuidv4 } from "uuid";
+import styled from "styled-components";
+import axios from 'axios';
 
-//   const handleSubmit = e =>{
-//       e.preventDefault()
-//       e.setReviews([...reviews, form])
+const secretKey = 'secret_AFKZAuWeh8KSRFU7dK4vcdUTEQG1pb3CyQtwBIdj9Ws'
 
-//   }
-  return (
-    <form /*onSubmit={handleSubmit}*/>
-        <h2>Leave Feedback</h2>
-            <label html="review">Review</label>
-            <textarea 
-                //value = {form.review} 
-                type= "text" 
-                placeholder="Leave Company Feedback" 
-                id="review" 
-                autoComplete='off'
-                //onChange={handleChange}
-            />
-            <button type="submit">Submit</button>
+const SubmitButton = styled.button `
+  cursor: pointer;
+  background-color: rgb(100, 0, 0);
+  padding: 1.5rem;
+  border-radius: 20px;
+  text-decoration: none;
+  color: white;
+  font-size: 40px;
+  font-family: "Lucida Console", "Courier New", monospace;
 
+  overflow-x: hidden;
+  float: center;
+`
+const database_id = '22f238cc864e4a1496e42e3d8a2c05c6';
+const Form = ({form, reviews, setForm, setReviews}) => {
+
+  const handleChange = e => {
+    const{name, value} = e.target;
+    setForm({...form, [name]: value});
+
+  }
+  const sendInfo = e => {
+    // todo
+  }
+  const checkValidity = () => {
+    // todo
+    return true;
+  }
+  const handleSubmit = e => {
+/*     console.log('BEGINING THE INITIAL TEST CALL!!!!!!!!!!!')
+    fetch('http://localhost:3002/' + database_id).then(async (resp) => {
+      console.log(resp);
+    });
+    console.log('DONE WITH INITIAL TEST THE CALL!!!!!!!!!!!')
+ */
+    fetch('http://localhost:3002/' + database_id, {
+      mode:'no-cors',
+      method: 'POST',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      body: JSON.stringify({
+        title: 'EMPTY TEST',
+        email: 'test@tamu.edu' 
+      }) 
+    });
+
+/*     if (checkValidity()) {
+      alert("The input data is good!");
+      e.preventDefault();
+      setReviews([...reviews, form]);
+      setForm({company: "", review: "", id: uuidv4()});
+    } else {
+      alert("Please enter valid information.")
+    } */
+  }
+
+  return (  
+    <form className="form" onSubmit={handleSubmit}>
+        <h2>Share Your Experience</h2>
+          <label htmlFor='Company'>Company</label><br/>
+          <select placeholder="Company Name"
+            id="company"
+            name="company"
+            value={form.company}
+            onChange={handleChange}>
+            <option value="None">Select Company</option>
+            <option value="Google">Google</option>
+            <option value="Facebook">Facebook</option>
+            <option value="Amazon">Amazon</option>
+            <option value="Netflix">Netflix</option>
+            <option value="Apple">Apple</option>
+            <option value="Microsoft">Microsoft</option>
+          </select><br/>
+          <label htmlFor='JobTitle'>Job Title</label><br/>
+          <select placeholder="Job Title"
+            id="job"
+            name="job"
+            value={form.job}
+            onChange={handleChange}>
+            <option value="None">Select Job Title</option>
+            <option value="Freshman Internship">Freshman Internship</option>
+            <option value="Sophomore Internship">Sophomore Internship</option>
+            <option value="Junior Internship">Junior Internship</option>
+            <option value="Senior Internship">Senior Internship</option>
+            <option value="Starting-Level Job (Front-End)">Starting-Level Job (Front-End)</option>
+            <option value="Starting-Level Job (Back-End)">Starting-Level Job (Back-End)</option>
+          </select><br/>
+          <label htmlFor='Name'>Name</label><br/>
+          <textarea
+                 value={form.name}
+                 placeholder="Enter your name"  
+                 id="name" 
+                 name="name" 
+                 autoComplete="off"
+                 onChange={handleChange}>
+          </textarea><br/>
+          <label htmlFor='Email'>Valid School Email</label><br/>
+          <textarea
+                 value={form.email}
+                 placeholder="Enter your valid school email"
+                 id="email" 
+                 name="email" 
+                 autoComplete="off"
+                 onChange={handleChange}>
+          </textarea><br/>
+          <label htmlFor='review'>Review</label><br/>
+          <div class="review-box"><textarea
+                 value={form.review}
+                 placeholder="Give feedback"  
+                 id="review" 
+                 name="review" 
+                 autoComplete="off"
+                 onChange={handleChange}>
+          </textarea></div><br/>
+          <SubmitButton onClick={handleSubmit}>
+            Submit
+          </SubmitButton>
     </form>
   )
 }
