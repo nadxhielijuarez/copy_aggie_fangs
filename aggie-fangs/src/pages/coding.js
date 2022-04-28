3bb722ada50bdd59869b35a89b868e54d41a7bb9
Joshua Wood <jwood@swood.org>
2022-04-28 10:05:28 -0500

    added radio button options for companies in problems page

--- aggie-fangs/src/pages/coding.js
+++ aggie-fangs/src/pages/coding.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import logo from "./../images/Aggie_Fangs_Logo_Transparent.png";
 import styled from "styled-components";
 
@@ -22,65 +22,155 @@ var probConcepts = <text></text>;
 var probPrompt = <text></text>;
 var probInOut = <text></text>;
 
-function getProblem () {
-    // todo
-    // this function will find the current problem and change what the page will output accordingly
-    probName = <text>String Reversal</text>;
-    probConcepts = <text>Traversal of Lists or Strings, Basic Problem Solving</text>;
-    probPrompt = <text>Given a string, write a function that reverses the string such that the old first element is now the last element, the old second element is now the second to last element, etc.</text>;
-    probInOut = <text>In: apple<br/>Out: elppa<br/>In: howdy<br/>Out: ydwoh</text>;
-}
 
-function changeProblem () {
-    // todo
-}
-
-function submitCode () {
-    // todo
-}
-
-function Coding() {
-    getProblem();
-    return (
-        <div>
-            <div class="coding coding-header">
-                <div class="split coding-logo">
-                    <img src={logo} alt="logo" />
-                </div>
-                <div class="split coding-problem-name">
-                    <h1>Problem Name: {probName}</h1>
-                    <h2>Concepts Covered: {probConcepts}</h2>
-                </div>
-            </div>
-            <div class="coding coding-main">
-                <div class="split problem-list">
-                    <h1>Problems:</h1>
-                    <label>
-                        <input type="checkbox" /> Sort By Company
-                    </label>
-                    <ul>
-                        <li><a href="" onClick={changeProblem()}>String Reversal</a></li>
-                    </ul>
-                </div>
-                <div class="split problem-statement">
-                    <h2>Problem Prompt:</h2>
-                    <body>{probPrompt}</body>
-                    <h2>Sample Inputs and Outputs:</h2>
-                    <body>{probInOut}</body>
-                    <h2>Your Code:</h2>
-                    <div class="coding-problem-left">
-                        <textarea class="code-window" id="userCode" name="userCode"/>
-                        <RunButton class="run-button">Run Code</RunButton>
+class Coding extends Component {
+    getProblem () {
+        // todo
+        // this function will find the current problem and change what the page will output accordingly
+        probName = <text>String Reversal</text>;
+        probConcepts = <text>Traversal of Lists or Strings, Basic Problem Solving</text>;
+        probPrompt = <text>Given a string, write a function that reverses the string such that the old first element is now the last element, the old second element is now the second to last element, etc.</text>;
+        probInOut = <text>In: apple<br/>Out: elppa<br/>In: howdy<br/>Out: ydwoh</text>;
+    }
+    handleChange(event) {
+        var thisCompany = event.target.value;
+        localStorage.setItem("this-company", thisCompany);
+        this.setState({
+            selectedOption: thisCompany
+        });
+    }
+    constructor() {
+        super();
+        var thisCompany = localStorage.getItem("this-company");
+        if (!thisCompany) {
+            thisCompany = "None";
+        }
+        this.state = {
+            name: "react",
+            selectedOption: thisCompany
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.getProblem();
+    }
+    render() {
+        return (
+            <div>
+                <div class="coding coding-header">
+                    <div class="split coding-logo">
+                        <img src={logo} alt="logo" />
                     </div>
-                    <div class="coding-problem-right">
-                        <textarea disabled class="compiler-window" id="terminal" name="terminal">
-                            Terminal Output
-                        </textarea>
+                    <div class="split coding-problem-name">
+                        <h1>Problem Name: {probName}</h1>
+                        <h2>Concepts Covered: {probConcepts}</h2>
                     </div>
                 </div>
+                <div class="coding coding-main">
+                    <div class="split problem-list">
+                        <h1>Problems:</h1>
+                        <form className='problem-sort'>
+                            <div className='radio'>
+                                <label>
+                                    <input
+                                        type = "radio"
+                                        value = "None"
+                                        checked = {this.state.selectedOption === "None"}
+                                        onChange = {this.handleChange}
+                                    />
+                                    None
+                                </label>
+                            </div>
+                            <div className='radio'>
+                                <label>
+                                    <input
+                                        type = "radio"
+                                        value = "Google"
+                                        checked = {this.state.selectedOption === "Google"}
+                                        onChange = {this.handleChange}
+                                    />
+                                    Google
+                                </label>
+                            </div>
+                            <div className='radio'>
+                                <label>
+                                    <input
+                                        type = "radio"
+                                        value = "Facebook"
+                                        checked = {this.state.selectedOption === "Facebook"}
+                                        onChange = {this.handleChange}
+                                    />
+                                    Facebook
+                                </label>
+                            </div>
+                            <div className='radio'>
+                                <label>
+                                    <input
+                                        type = "radio"
+                                        value = "Amazon"
+                                        checked = {this.state.selectedOption === "Amazon"}
+                                        onChange = {this.handleChange}
+                                    />
+                                    Amazon
+                                </label>
+                            </div>
+                            <div className='radio'>
+                                <label>
+                                    <input
+                                        type = "radio"
+                                        value = "Netflix"
+                                        checked = {this.state.selectedOption === "Netflix"}
+                                        onChange = {this.handleChange}
+                                    />
+                                    Netflix
+                                </label>
+                            </div>
+                            <div className='radio'>
+                                <label>
+                                    <input
+                                        type = "radio"
+                                        value = "Apple"
+                                        checked = {this.state.selectedOption === "Apple"}
+                                        onChange = {this.handleChange}
+                                    />
+                                    Apple
+                                </label>
+                            </div>
+                            <div className='radio'>
+                                <label>
+                                    <input
+                                        type = "radio"
+                                        value = "Microsoft"
+                                        checked = {this.state.selectedOption === "Microsoft"}
+                                        onChange = {this.handleChange}
+                                    />
+                                    Microsoft
+                                </label>
+                            </div>
+                        </form>
+
+                        <ul>
+                            <li><a href="">String Reversal</a></li>
+                        </ul>
+                    </div>
+                    <div class="split problem-statement">
+                        <h2>Problem Prompt:</h2>
+                        <body>{probPrompt}</body>
+                        <h2>Sample Inputs and Outputs:</h2>
+                        <body>{probInOut}</body>
+                        <h2>Your Code:</h2>
+                        <div class="coding-problem-left">
+                            <textarea class="code-window" id="userCode" name="userCode"/>
+                            <RunButton class="run-button">Run Code</RunButton>
+                        </div>
+                        <div class="coding-problem-right">
+                            <textarea disabled class="compiler-window" id="terminal" name="terminal">
+                                Terminal Output
+                            </textarea>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
-export default Coding
+export default Coding;
