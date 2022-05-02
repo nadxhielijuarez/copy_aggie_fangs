bc5959042fe9e73a21e012356c5fc9a7f0b81507
nadxhieli <84687270+nadxhielijuarez@users.noreply.github.com>
2022-05-02 17:18:25 -0500

    finishing up, beginning endpoint reroute

--- aggie-fangs/src/App.js
+++ aggie-fangs/src/App.js
@@ -83,7 +83,7 @@ window.onload = function() {
     localStorage.setItem("this-company", "None");
   }
   var localDBAddress = "http://localhost:3002";
-  var publicDBAddress = "184.92.91.83:3002";
+  var publicDBAddress = "http://35.208.174.43:3002/";
   localStorage.setItem("db-address", localDBAddress);
   var localCompAddress = "http://localhost:3001";
   var publicCompAddress = "184.92.91.83:3001";
